c969922096e2a713bb45b48eca910dcbaa2222ab
WillAnjos <willanjos@hotmail.com>
2019-09-05 12:06:41 -0300

    set up gulp to refresh browser automatically

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require("gulp"),
   autoprefixer = require("autoprefixer"),
   postcss = require("gulp-postcss"),
   cssvars = require("postcss-simple-vars"),
-  nested = require("postcss-nested");
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
 gulp.task("default", function() {
   console.log("YEEEHUUUUU - YOU CREAATE A GULP FILE TASK.");
@@ -16,11 +17,13 @@ gulp.task("html", function() {
 gulp.task("styles", function() {
   return gulp
     .src("./app/assets/styles/style.css")
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", () => {
   watch("./app/index.html", gulp.series("html"));
   watch("./app/assets/styles/style.css", gulp.series("styles"));
+  watch("./app/assets/styles/modules/_large-hero.css", gulp.series("styles"));
+  watch("./app/assets/styles/base/_global.css", gulp.series("styles"));
 });
